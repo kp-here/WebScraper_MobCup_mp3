957585f5d19849cb993d2b8efa290b01f680195f
Abhiram K P <73926551+kp-here@users.noreply.github.com>
2021-11-10 02:39:02 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,13 @@
 import requests
 import re
 import os
-from bs4 import BeautifulSoup as bs
-searchword=input("Enter Song to Search\n\n")
 import urllib.parse
+from bs4 import BeautifulSoup as bs
 
+searchword=input("Enter Song to Search\n\n")
 new = requests.get("https://mobcup.net/search?q="+urllib.parse.quote(searchword)+"&type=ringtone")
 soup = bs(new.content, "html.parser")
+
 for i in soup.find_all('div',class_='item ringtone'):
     subpagelink="https://mobcup.net"+str(i.find('a')['href'])
     subpage=bs(requests.get(subpagelink).content,"html.parser")
@@ -16,9 +17,8 @@ for i in soup.find_all('div',class_='item ringtone'):
             song=requests.get(a)
             name=" ".join(a.split("/")[-3].split("-")[:-1]).replace('-',' ')+".mp3"
             with open(os.path.join('T:\Web Scraping\Downloads',name), "wb") as f:
-                #add the absolute location above
+                #add the absolute location of the directory above
                 f.write(song.content)
             print("\n"+a+" Done")
 
 print("\nFinished!")
-    # print(subpage.find('audio'))
